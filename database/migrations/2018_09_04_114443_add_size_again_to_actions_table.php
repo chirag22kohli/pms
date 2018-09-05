80cf921cee2044c1d68d80f9e26ece848e4b0bfe
unknown <chirag22.kohli@gmail.com>
2018-09-05 14:48:26 +0530

    migissue

--- database/migrations/2018_09_04_114443_add_size_again_to_actions_table.php
+++ database/migrations/2018_09_04_114443_add_size_again_to_actions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddSizeToActionsTable extends Migration {
+class AddSizeAgainToActionsTable extends Migration {
 
     /**
      * Run the migrations.
