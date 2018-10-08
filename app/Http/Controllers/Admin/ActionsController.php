b94550121d39c55c5469ae8a137ae3ec34f27286
unknown <chirag22.kohli@gmail.com>
2018-10-08 16:09:58 +0530

    ee

--- app/Http/Controllers/Admin/ActionsController.php
+++ app/Http/Controllers/Admin/ActionsController.php
@@ -298,7 +298,7 @@ class ActionsController extends Controller {
         endif;
         return json_encode([
             'success' => '1',
-            'datas'=>public_path($imagePath)
+            'datas'=>$datas
         ]);
     }
 
