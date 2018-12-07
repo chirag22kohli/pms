82dc1f5204ffb55712bfb1379924b5149eba57f3
Chirag Kohli <chirag22.kohli@gmail.com>
2018-12-07 16:51:40 +0530

    graph

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -41,9 +41,9 @@
             console.log(data);
             $.each(data, function (key, value) {
                 console.log(value['Month(created_at)']);
-                dataPoints.push({x: new Date(value['Year(created_at)'], value['Month(created_at)']), y: parseInt(value['sum'])});
+                dataPoints.push({x: new Date(value['Year(created_at)'], value['Month(created_at)']), y: value['sum']});
             });
-
+            console.log(options);
             chart.render();
         });
         var options = {
