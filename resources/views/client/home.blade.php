16bd29b3b17fc24b6eb5ce78520a4aa16c7a77d7
Chirag Kohli <chirag22.kohli@gmail.com>
2018-12-07 17:17:52 +0530

    graph

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -63,7 +63,7 @@
                 valueFormatString: "#,###"
             },
             data: [{
-                    type: "area",
+                    type: "column",
                     color: "rgba(0,75,141,0.7)",
                     yValueFormatString: "$#,###.##",
                     dataPoints: dataPoints
