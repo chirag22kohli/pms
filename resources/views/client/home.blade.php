1a9c34e161eca1f3ce4125cb17e420bbbd16a333
Chirag Kohli <chirag22.kohli@gmail.com>
2018-12-07 16:55:21 +0530

    graph

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -61,7 +61,7 @@
             },
             data: [{
                     yValueFormatString: "$#,###",
-                    xValueFormatString: "MMMM",
+                    xValueFormatString: "M",
                     type: "spline",
                     dataPoints: dataPoints
                 }]
