b6ef24603fc275f30e02ae7da4971bbf851a614b
Chirag kohli <chiragkohli@Chirags-MacBook-Air.local>
2019-09-25 23:57:49 +0530

    ecomapp work

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\Controller;
 use App\User;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 use Validator;
 use DB;
 use App\Role;
